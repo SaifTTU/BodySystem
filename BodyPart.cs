34ad849fda060e37ddbded33a5d0f29ba1da6cea
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2022-01-18 12:50:24 -0600

    BodyPart.cs
    This is my first draft of what the body part object class would look like.
    I think it would get stored in a BodyParts class or something.
    Additionally wondering if we need to make getters and setters if we are using Unity.

---
+++ BodyPart.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public string name;
+    public string description;
+    public string secondaryDescription;
+
+    public List<GameObject> joint = new List<GameObject>();
+
+    bool jointsCreated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollision(Collision collision){
+        if(collision.gameObject.tag=="BodyPart"){
+            if(jointsCreated==false)    
+                CreateJoint(collision.gameObject, gameObject);
+            
+        }
+    }
+
+    void CreateJoint(GameObject toBeAttached, GameObject self){
+        //if(myList.Contains(x))
+        //if(joint.Contains(toBeAttached)==false){
+            joint.Add(toBeAttached);
+        //}
+        //else
+        //{
+            jointsCreated = true;
+        //}
+        
+    }
+}
